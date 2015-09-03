e11e6763352e4815de0b3aae5e859754a71f7530
mahesh maurya <mahesh@wohlig.com>
2015-09-03 22:19:01 +0530

    no faltu css

--- application/views/backend/header.php
+++ application/views/backend/header.php
@@ -7,49 +7,24 @@
     <title>Blazen |
         <?php echo $title; ?>
     </title>
-    <!--    <link rel="icon" href="<?php echo base_url('assets/img/camp_logo.png'); ?>" type="image/x-icon" />-->
-    <link rel="stylesheet" href="http://css-spinners.com/css/spinner/dots.css" type="text/css">
     <link rel="icon" href="<?php echo base_url('assets/favicon.ico'); ?>" type="image/x-icon" />
     <link href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>" rel="stylesheet">
     <link href="<?php echo base_url('assets/css/nifty.min.css'); ?>" rel="stylesheet">
     <link href="<?php echo base_url('assets/css/bootstrap-table.min.css'); ?>" rel="stylesheet">
-    <!--external css-->
-    <link href="<?php echo base_url('assets/assets/font-awesome/css/font-awesome.css'); ?>" rel="stylesheet" />
-    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-    <!-- Custom styles for this template -->
-    <link href="<?php echo base_url('assets/css/themes/type-b/theme-navy.min.css'); ?>" rel="stylesheet">
+
     <link href="<?php echo base_url('assets/plugins/font-awesome/css/font-awesome.min.css'); ?>" rel="stylesheet">
-    <link href="<?php echo base_url('assets/plugins/animate-css/animate.min.css'); ?>" rel="stylesheet">
-    <link href="<?php echo base_url('assets/plugins/morris-js/morris.min.css'); ?>" rel="stylesheet">
-    <link href="<?php echo base_url('assets/plugins/switchery/switchery.min.css'); ?>" rel="stylesheet">
     <link href="<?php echo base_url('assets/plugins/bootstrap-select/bootstrap-select.min.css'); ?>" rel="stylesheet">
-    <link href="<?php echo base_url('assets/css/demo/nifty-demo.min.css'); ?>" rel="stylesheet">
     <link href="<?php echo base_url('assets/plugins/pace/pace.min.css'); ?>" rel="stylesheet">
-    <!--    <script src="http://code.jquery.com/jquery-1.9.1.js"></script>-->
     <script src="<?php echo base_url('assets/js/jquery-2.1.1.min.js'); ?>" type="text/javascript"></script>
-    <link href="<?php echo base_url('assets/css/style-responsive.css'); ?>" rel="stylesheet" />
-    <link href="<?php echo base_url('assets/assets/jquery-easy-pie-chart/jquery.easy-pie-chart.css'); ?>" rel="stylesheet" type="text/css" media="screen" />
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/owl.carousel.css'); ?>" type="text/css">
-    <link href="<?php echo base_url('assets/css/TableTools.css'); ?>" rel="stylesheet">
-    <link href="<?php echo base_url('assets/css/demo_table.css'); ?>" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/assets/bootstrap-datepicker/css/datepicker.css'); ?>" />
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/assets/bootstrap-colorpicker/css/colorpicker.css'); ?>" />
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/assets/bootstrap-daterangepicker/daterangepicker.css'); ?>" />
+    
+   
     <link rel="stylesheet" href="<?php echo base_url('assets/css/select2.css'); ?>" type="text/css" />
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/custom.css'); ?>" type="text/css" />
-    <!--	<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>-->
-    <!--	 <script src="<?php echo base_url('assets/js/jquery-1.8.3.min.js'); ?>" type="text/javascript"></script>-->
-    <script src="<?php echo base_url('assets/js/jquery.sparkline.js'); ?>" type="text/javascript"></script>
-    <script src="<?php echo base_url('assets/assets/jquery-easy-pie-chart/jquery.easy-pie-chart.js'); ?> "></script>
-    <script src="<?php echo base_url('assets/js/sparkline-chart.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/js/easy-pie-chart.js'); ?>"></script>
+
     <script src="<?php echo base_url('assets/js/chintantable.js'); ?>"></script>
     <script src="<?php echo base_url('assets/js/select2.js'); ?>"></script>
     <script src="<?php echo base_url('assets/js/masonry.pkgd.min.js'); ?>"></script>
     <script src="<?php echo base_url('assets/js/main.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/js/nifty.min.js'); ?>"></script>
     <script src="<?php echo base_url('assets/plugins/pace/pace.min.js'); ?>"></script>
-<!--    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700&amp;subset=latin" rel="stylesheet">-->
     <script type="text/javascript">
         var base_url = "<?php echo base_url(); ?>";
         newbase_url = base_url;
@@ -112,86 +87,6 @@
     </script>
 
 </head>
-<!--STYLESHEET-->
-<!--=================================================-->
-
-<!--Open Sans Font [ OPTIONAL ] -->
-<!-- 	<link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700&amp;subset=latin" rel="stylesheet">-->
-
-
-<!--Bootstrap Stylesheet [ REQUIRED ]-->
-<!--	<link href="css/bootstrap.min.css" rel="stylesheet">-->
-
-
-<!--Nifty Stylesheet [ REQUIRED ]-->
-<!--	<link href="css/nifty.min.css" rel="stylesheet">-->
-
-
-<!--Font Awesome [ OPTIONAL ]-->
-<!--	<link href="plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet">-->
-
-
-<!--Animate.css [ OPTIONAL ]-->
-<!--	<link href="plugins/animate-css/animate.min.css" rel="stylesheet">-->
-
-
-<!--Morris.js [ OPTIONAL ]-->
-<!--	<link href="plugins/morris-js/morris.min.css" rel="stylesheet">-->
-
-
-<!--Switchery [ OPTIONAL ]-->
-<!--	<link href="plugins/switchery/switchery.min.css" rel="stylesheet">-->
-
-
-<!--Bootstrap Select [ OPTIONAL ]-->
-<!--	<link href="plugins/bootstrap-select/bootstrap-select.min.css" rel="stylesheet">-->
-
-
-<!--Demo script [ DEMONSTRATION ]-->
-<!--	<link href="css/demo/nifty-demo.min.css" rel="stylesheet">-->
-
-
-
-
-<!--SCRIPT-->
-<!--=================================================-->
-
-<!--Page Load Progress Bar [ OPTIONAL ]-->
-<!--
-	<link href="plugins/pace/pace.min.css" rel="stylesheet">
-	<script src="plugins/pace/pace.min.js"></script>
--->
-
-
-
-<!--
-
-	REQUIRED
-	You must include this in your project.
-
-	RECOMMENDED
-	This category must be included but you may modify which plugins or components which should be included in your project.
-
-	OPTIONAL
-	Optional plugins. You may choose whether to include it in your project or not.
-
-	DEMONSTRATION
-	This is to be removed, used for&nbsp;demonstration purposes only.&nbsp;This category must not be included in your project.
-
-	SAMPLE
-	Some script samples which explain how to initialize plugins or components. This category should not be included in your project.
-
-
-	Detailed information and more samples can be found in the document.
-
-	-->
-
-
-</head>
-
-<!--TIPS-->
-<!--You may remove all ID or Class names which contain "demo-", they are only used for demonstration. -->
-
 <body>
     <div id="container" class="effect mainnav-lg">
 
@@ -264,7 +159,7 @@
                                             <li>
                                                 <a href="#" class="media">
                                                     <div class="media-left">
-                                                        <img src="<?php echo base_url('assets/img/avi4.png'); ?>" alt="Profile Picture" class="img-circle img-sm">
+                                                        <img src="<?php echo base_url('assets/img/av4.png'); ?>" alt="Profile Picture" class="img-circle img-sm">
                                                     </div>
                                                     <div class="media-body">
                                                         <div class="text-nowrap">Lucy sent you a message</div>
